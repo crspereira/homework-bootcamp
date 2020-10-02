7e8e46f179f46c186b42fd4c0e28b28b8a2502d8
Clayton Pereira <clayton.pereira@outlook.com>
2020-10-02 19:08:57 -0300

    Client Resource Insert

--- backend/src/main/java/com/devsuperior/homework/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/homework/services/ClientService.java
@@ -28,10 +28,28 @@ public class ClientService implements Serializable {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
+	
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ServiceNotFoundExeception("Client Entity ID Not Found!"));
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	//metodos auxiliares
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 }
